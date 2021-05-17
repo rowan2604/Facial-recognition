cb42507e4877180d9a4574a3335e9451784946a2
Rémy Cspl <45265766+DaneX271@users.noreply.github.com>
2021-05-17 12:04:23 +0200

    Add files via upload

--- AddToBDD.py
+++ AddToBDD.py
@@ -1,5 +1,5 @@
-import sqlite3
 from flask import Flask, render_template, request
+import mysql.connector
 
 def insertBlob():
     try:
@@ -7,12 +7,12 @@ def insertBlob():
         prenom = request.form['prenom']
         promo = request.form['promo']
         photo = "./images/" + request.form['photo']
-        conn = sqlite3.connect('etudiant.db')
+        conn = mysql.connector.connect(host="eu-cdbr-west-01.cleardb.com", user="b8523d276180fb", password="e548c5fe", database="heroku_432d5a7d6f44b44")
         conn.text_factory = str
         cur = conn.cursor()
         print("Connexion reussie à SQLite")
-        cur.execute("CREATE TABLE IF NOT EXISTS ETUDIANT (id INTEGER PRIMARY KEY AUTOINCREMENT, Nom TEXT NOT NULL, Prenom TEXT NOT NULL, Promo TEXT NOT NULL, Etat INTEGER NOT NULL, Photo BLOB NOT NULL)")
-        sql = "INSERT INTO ETUDIANT (nom, prenom, promo, etat, photo) VALUES (?,?,?,?,?)"
+        cur.execute("CREATE TABLE IF NOT EXISTS Etudiant (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, Nom VARCHAR(100) NOT NULL, Prenom VARCHAR(100) NOT NULL, Promo VARCHAR(100) NOT NULL, Etat INT NOT NULL, Photo BLOB NOT NULL)")
+        sql = "INSERT INTO Etudiant (Nom, Prenom, Promo, Etat, Photo) VALUES (%s,%s,%s,%s,%s)"
 
         with open(photo, 'rb') as myfile:
             blobFile= myfile.read()
@@ -25,7 +25,7 @@ def insertBlob():
         conn.close()
         print("Connexion SQLite est fermee")
 
-    except sqlite3.Error as error:
+    except mysql.connector.Error as error:
         print("Erreur lors de l'insertion", error)
 
 insertBlob()
--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import sqlite3
 from flask import Flask, render_template, request
+import mysql.connector
 
 app = Flask(__name__)
 
