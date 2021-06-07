cc3b96bfff2751c9dc842f0f2a16e749e4cb0148
Rémy Cspl <45265766+DaneX271@users.noreply.github.com>
2021-06-07 15:42:15 +0200

    Interface web - graphe

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def connexion():
 def auth():
     identifiant = request.form['Identifiant']
     mdp = request.form['mdp']
-    conn = mysql.connector.connect(host="eu-cdbr-west-01.cleardb.com", user="b8523d276180fb", password="e548c5fe", database="heroku_432d5a7d6f44b44")
+    conn = mysql.connector.connect(host="eu-cdbr-west-01.cleardb.com", user="bc534e43745e55", password="3db62771", database="heroku_642c138889636e7")
     conn.text_factory = str
     cur = conn.cursor()
     cur.execute("SELECT password FROM connexion WHERE identifiant = '" + identifiant + "'")
@@ -45,10 +45,11 @@ def deco():
 @app.route('/index')
 def index():
     if(request.remote_addr in session ):
-        conn = mysql.connector.connect(host="eu-cdbr-west-01.cleardb.com", user="b8523d276180fb", password="e548c5fe", database="heroku_432d5a7d6f44b44")
+        conn = mysql.connector.connect(host="eu-cdbr-west-01.cleardb.com", user="bc534e43745e55", password="3db62771", database="heroku_642c138889636e7")
         conn.text_factory = str
         cur = conn.cursor()
         print("Connexion reussie à SQLite")
+        cur.execute("CREATE TABLE IF NOT EXISTS Etudiant (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, Nom VARCHAR(100) NOT NULL, Prenom VARCHAR(100) NOT NULL, Promo VARCHAR(100) NOT NULL, Presence INT NOT NULL, Photo1 BLOB NOT NULL, Photo2 BLOB NOT NULL, Photo3 BLOB NOT NULL, Photo4 BLOB NOT NULL, Photo5 BLOB NOT NULL, Photo6 BLOB NOT NULL, Photo7 BLOB NOT NULL)")
         cur.execute("SELECT * FROM Etudiant")
         posts = cur.fetchall()
         cur.close()
@@ -76,27 +77,28 @@ def ajouterEtRetour():
             pic1 = request.files['photo1']
             pic2 = request.files['photo2']
             pic3 = request.files['photo3']
-            conn = mysql.connector.connect(host="eu-cdbr-west-01.cleardb.com", user="b8523d276180fb", password="e548c5fe", database="heroku_432d5a7d6f44b44")
+            pic4 = request.files['photo4']
+            pic5 = request.files['photo5']
+            pic6 = request.files['photo6']
+            pic7 = request.files['photo7']
+            conn = mysql.connector.connect(host="eu-cdbr-west-01.cleardb.com", user="bc534e43745e55", password="3db62771", database="heroku_642c138889636e7")
             conn.text_factory = str
             cur = conn.cursor()
             print("Connexion reussie à SQLite")
-            cur.execute("CREATE TABLE IF NOT EXISTS Etudiant (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, Nom VARCHAR(100) NOT NULL, Prenom VARCHAR(100) NOT NULL, Promo VARCHAR(100) NOT NULL, Presence INT NOT NULL, Photo1 BLOB NOT NULL, Photo2 BLOB NOT NULL, Photo3 BLOB NOT NULL)")
-            sql = "INSERT INTO Etudiant (Nom, Prenom, Promo, Presence, Photo1, Photo2, Photo3) VALUES (%s,%s,%s,%s,%s,%s,%s)"
-            print(pic1)
-            print(pic2)
-            print(pic3)
+            sql = "INSERT INTO Etudiant (Nom, Prenom, Promo, Presence, Photo1, Photo2, Photo3, Photo4, Photo5, Photo6, Photo7) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
             blobFile1=pic1.read()
             blobFile2=pic2.read()
             blobFile3=pic3.read()
-            value = (nom, prenom, promo, 0, blobFile1, blobFile2, blobFile3)
+            blobFile4=pic4.read()
+            blobFile5=pic5.read()
+            blobFile6=pic6.read()
+            blobFile7=pic7.read()
+            value = (nom, prenom, promo, 0, blobFile1, blobFile2, blobFile3, blobFile4, blobFile5, blobFile6, blobFile7)
             cur.execute(sql, value)
             conn.commit()
             print("Fichier insere avec succes")
-            cur.execute( "SELECT Nom, Prenom, Promo FROM Etudiant WHERE id =(SELECT MAX(id) FROM Etudiant)")
+            cur.execute( "SELECT Nom, Prenom, Promo FROM Etudiant WHERE id = (SELECT MAX(id) FROM Etudiant)")
             validation = cur.fetchone()
-            print(validation[0])
-            print(validation[1])
-            print(validation[2])
             cur.close()
             conn.close()
             print("Connexion SQLite est fermee")
@@ -120,13 +122,11 @@ def ajouterIdentifiant():
         identifiant = request.form['identifiant']
         password = request.form['password']
         pw_hash = bcrypt.generate_password_hash(password)
-        print(identifiant)
-        print(pw_hash)
-        conn = mysql.connector.connect(host="eu-cdbr-west-01.cleardb.com", user="b8523d276180fb", password="e548c5fe", database="heroku_432d5a7d6f44b44")
+        conn = mysql.connector.connect(host="eu-cdbr-west-01.cleardb.com", user="bc534e43745e55", password="3db62771", database="heroku_642c138889636e7")
         conn.text_factory = str
         cur = conn.cursor()
         print("Connexion reussie à SQLite")
-        sql = "INSERT INTO connexion (identifiant, password) VALUES (%s,%s)"
+        sql = "INSERT INTO connexion (Identifiant, Password) VALUES (%s,%s)"
         value = (identifiant, pw_hash)
         cur.execute(sql, value)
         conn.commit()
@@ -139,3 +139,52 @@ def ajouterIdentifiant():
         print("Erreur lors de l'insertion", error)
     
     return render_template("validation.html")
+
+@app.route('/annee', methods=['POST'])
+def annee():
+    try :
+        if(request.remote_addr in session ):
+            annee = request.form['annee']
+            annees = []
+            annees.append(annee)
+            conn = mysql.connector.connect(host="eu-cdbr-west-01.cleardb.com", user="bc534e43745e55", password="3db62771", database="heroku_642c138889636e7")
+            conn.text_factory = str
+            cur = conn.cursor()
+            print("Connexion reussie à SQLite")
+            if annees[0] == '4':
+                cur.execute("SELECT * FROM Etudiant WHERE (Promo = 'M1')")
+
+            elif annees[0] == '5':
+                cur.execute("SELECT * FROM Etudiant WHERE (Promo = 'M2')")
+
+            else :
+                cur.execute("SELECT * FROM Etudiant WHERE (Promo = 'CIR" + annee + "' OR Promo = 'CPG" + annee + "' OR Promo = 'CNB" + annee + "')")
+            posts = cur.fetchall()
+            return render_template('annee.html', posts = posts, annee = annees)
+
+        else:
+            return redirect('/')
+    
+    except mysql.connector.Error as error:
+        print("Erreur lors de l'insertion", error)
+
+@app.route('/promo', methods=['POST'])
+def promo():
+    try :
+        if(request.remote_addr in session ):
+            promo = request.form['promo']
+            promos = []
+            promos.append(promo)
+            conn = mysql.connector.connect(host="eu-cdbr-west-01.cleardb.com", user="bc534e43745e55", password="3db62771", database="heroku_642c138889636e7")
+            conn.text_factory = str
+            cur = conn.cursor()
+            print("Connexion reussie à SQLite")
+            cur.execute("SELECT * FROM Etudiant WHERE (Promo = '" + promo + "')")
+            posts = cur.fetchall()
+            return render_template('promo.html', posts = posts, promo = promos)
+
+        else:
+            return redirect('/')
+    
+    except mysql.connector.Error as error:
+        print("Erreur lors de l'insertion", error)
