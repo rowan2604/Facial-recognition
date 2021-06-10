05e6af29fea679cafedc1b7f653ae93e016f1756
Abdel <abderzak.kaddouri@isen.yncrea.fr>
2021-06-10 13:43:47 +0200

    V4

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 bcrypt = Bcrypt(app)
 app.secret_key = os.environ.get('SECRET') #sur heroku var
 #app.secret_key = 'SECRET_KEY' #en local
-app.permanent_session_lifetime = timedelta(minutes = 30)
+#app.permanent_session_lifetime = timedelta(minutes = 30)
 
 def lancer():
     stream = open("AddToBDD.py")
