00d608e2fd23dbda0ec8460d1176839002c755ae
Abdel <abderzak.kaddouri@isen.yncrea.fr>
2021-06-14 15:05:36 +0200

    code commenté

--- app.py
+++ app.py
@@ -16,57 +16,60 @@ app.permanent_session_lifetime = timedelta(minutes = 30)
 @app.route('/')
 def connexion():
     remote_addr = request.headers.get("X-Forwarded-For")
-    print("C'EST LA")
-    print(remote_addr)
-    #print(request.environ['REMOTE_ADDR'])
-    print(request.headers.get("X-Forwarded-For"))
-    if (request.headers.get("X-Forwarded-For") in session):
-        return redirect('/index')
+    if (request.headers.get("X-Forwarded-For") in session):# On vérifie si on a déjà une session ouverte 
+        return redirect('/index')# Si on est déjà connecté on ouvre directement la page index
     else:
-        return render_template('login.html')
+        return render_template('login.html')# Sinon on lance la page de connexion au site
 
+# Page servant à vérifier si la combinaison identifiant/mot de passe coïncident bien avec une déjà présente dans la BDD
 @app.route('/auth/', methods=['POST'])
 def auth():
+     # On réupère les différentes valeurs entré dans les champs de noter formulaire
     identifiant = request.form['Identifiant']
     mdp = request.form['mdp']
+    # On se connecte ensuite à notre BDD en renseigannt toutes les information nécessaires
     conn = mysql.connector.connect(host="eu-cdbr-west-01.cleardb.com", user="bc534e43745e55", password="3db62771", database="heroku_642c138889636e7")
-    conn.text_factory = str
-    cur = conn.cursor()
-    cur.execute("SELECT password FROM connexion WHERE identifiant = '" + identifiant + "'")
-    BDDmdp = cur.fetchone()
-    cur.close()
-    conn.close()
+    conn.text_factory = str # On définit notre connexion à la BDD comme un type string
+    cur = conn.cursor()#On crée un curseur qui nous servira à effectuer toutes les requêtes SQL nécessaires
+    cur.execute("SELECT password FROM connexion WHERE identifiant = '" + identifiant + "'") # On récupère alors le mot de passe qui correspond à l'identifiant rentré dans notre formulaire
+    BDDmdp = cur.fetchone()# On stocke le mot de passe dans une variable de type tableau
+    cur.close()# On ferme le curseur 
+    conn.close()# On ferme la connexion
+    #On vérifie que le mot de passe rentré dans notre formulaire correspond bien à celui récupéré dans notre BDD
+    # Bien évidemment tous nos mots de passe sont alors cryptés à l'aide de la méthode BCrypt
     if BDDmdp != None and bcrypt.check_password_hash(BDDmdp[0], mdp):
-        print("IF")
-        session[request.headers.get("X-Forwarded-For")] = identifiant
-        return redirect('/index')
+        session[request.headers.get("X-Forwarded-For")] = identifiant# On stocke alors notre identifiant dans notre dictionnaire session afin de le réutiliser plus tard
+        return redirect('/index')# Une fois connecté on est alors redirigé vers la page index 
     else:
-        print("ELSE")
-        return redirect('/')
+        return redirect('/')# Si les mots de passe ou les identifiants ne coïncident pas, la page de connexion est alors rafraîchie 
 
+
+# Page servant à se déconnecté de la session courante
 @app.route('/deconnexion')
 def deco():
-    session.pop(request.headers.get("X-Forwarded-For"), None)
-    return redirect('/')
+    session.pop(request.headers.get("X-Forwarded-For"), None) # On ferme la session
+    return redirect('/')# On est alors redirigé vers la page de connexion
 
+# Page affichant la liste entière des étudiants présents dans notre BDD
 @app.route('/index')
 def index():
-    if(request.headers.get("X-Forwarded-For") in session ):
+    if(request.headers.get("X-Forwarded-For") in session ):# On vérifie que l'utilisateur est bien connecté à la session afin de ne pas pouvoir accéder à la page sans être connecté d'avance
         conn = mysql.connector.connect(host="eu-cdbr-west-01.cleardb.com", user="bc534e43745e55", password="3db62771", database="heroku_642c138889636e7")
         conn.text_factory = str
         cur = conn.cursor()
-        print("Connexion reussie à SQLite")
+        # On va alors créer une table dans notre BDD qui contiendra la liste de tous nos étudiants
         cur.execute("CREATE TABLE IF NOT EXISTS Etudiant (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, Nom VARCHAR(100) NOT NULL, Prenom VARCHAR(100) NOT NULL, Promo VARCHAR(100) NOT NULL, Presence VARCHAR(45) NOT NULL, Photo1 BLOB NOT NULL, Photo2 BLOB NOT NULL, Photo3 BLOB NOT NULL, Photo4 BLOB NOT NULL, Photo5 BLOB NOT NULL, Photo6 BLOB NOT NULL, Photo7 BLOB NOT NULL)")
-        cur.execute("SELECT * FROM Etudiant")
-        posts = cur.fetchall()
+        cur.execute("SELECT * FROM Etudiant") # On récupère alors la liste de tous nos étudiants dans le curseur
+        posts = cur.fetchall()# On place alors les éléments du curseur dans une variable de type tuple
         cur.close()
         conn.close()
-        print("Connexion SQLite est fermee")
+        # On affiche la page correspondante au lancement de la page, et on envoie les valeurs de noter posts à la page html
         return render_template('dashboard.html', posts = posts)
 
     else:
         return redirect('/')  
 
+# Page affichant le dormulaire servant à ajouter des étudiants à la BDD
 @app.route('/ajouter')
 def pageAjouter():
     if(request.headers.get("X-Forwarded-For") in session ):
@@ -74,6 +77,7 @@ def pageAjouter():
     else:
         return redirect('/')
 
+# Page de vérification de l'ajout d'un étudiant
 @app.route('/validation', methods=['POST'])
 def ajouterEtRetour():
     if(request.headers.get("X-Forwarded-For") in session ):
@@ -91,8 +95,9 @@ def ajouterEtRetour():
             conn = mysql.connector.connect(host="eu-cdbr-west-01.cleardb.com", user="bc534e43745e55", password="3db62771", database="heroku_642c138889636e7")
             conn.text_factory = str
             cur = conn.cursor()
-            print("Connexion reussie à SQLite")
+            # On stocke la commande SQL à effectuer pour ajouter notre étudiant à la BDD afin de ne pas avoir une commande trop longue par la suite
             sql = "INSERT INTO Etudiant (Nom, Prenom, Promo, Presence, Photo1, Photo2, Photo3, Photo4, Photo5, Photo6, Photo7) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+            # On convertit toutes nos photos en BlobFile afin de pouvoir les stocker convenablement en BDD
             blobFile1 = pic1.read()
             blobFile2 = pic2.read()
             blobFile3 = pic3.read()
@@ -100,21 +105,21 @@ def ajouterEtRetour():
             blobFile5 = pic5.read()
             blobFile6 = pic6.read()
             blobFile7 = pic7.read()
+            # On vérifie si il n'existe pas un étudiant avec la même combinaison Nom/Prenom dans la BDD et dans celui que l'on veut ajouter 
             cur.execute("SELECT * FROM etudiant WHERE Nom LIKE '" + nom + "%' AND Prenom LIKE '" + prenom + "%'")
-            long = len(cur.fetchall())
-            print(len(cur.fetchall()))
+            # Si jamais on trouve un étudiant avec la même combinaison de Nom/Prenom, on lui ajoute un numéro à la fin pour le différencier des autres et éviter d'avoir des doublons en BDD
             if long > 0:
-                nom = nom + str(long)
+                nom = nom + str(long) # On ajoute la valeur de notre long, convertit en string, à noter variable de base
                 prenom = prenom + str(long)
+            # On définit le set de valeurs à envoyer dans la BDD
             value = (nom, prenom, promo, '$', blobFile1, blobFile2, blobFile3, blobFile4, blobFile5, blobFile6, blobFile7)
             cur.execute(sql, value)
             conn.commit()
-            print("Fichier insere avec succes")
+             # On  récupère le informations du dernier étudiant ajouté en BDD afin de vérifier qu'il a bien été ajouté dans la BDD et que l'utilisateur puisse vérifier les informations rentrées
             cur.execute( "SELECT Nom, Prenom, Promo FROM Etudiant WHERE id = (SELECT MAX(id) FROM Etudiant)")
             validation = cur.fetchone()
             cur.close()
             conn.close()
-            print("Connexion SQLite est fermee")
 
         except mysql.connector.Error as error:
             print("Erreur lors de l'insertion", error)
@@ -124,35 +129,35 @@ def ajouterEtRetour():
     else:
         return redirect('/')
 
-
+# Page nous permettant d'ajouter des utilisateurs
 @app.route("/ajouterCO")
 def ajouterCo():
     return render_template("ajouterCo.html")
 
+# Page permettant d'ajouter l'utilisateur à la table connexion de notre BDD
 @app.route('/validCo', methods=['POST'])
 def ajouterIdentifiant():
     try :
         identifiant = request.form['identifiant']
         password = request.form['password']
-        pw_hash = bcrypt.generate_password_hash(password)
+        pw_hash = bcrypt.generate_password_hash(password)# On crypte notre mot de passe à l'aide de la méthode BCrypt
         conn = mysql.connector.connect(host="eu-cdbr-west-01.cleardb.com", user="bc534e43745e55", password="3db62771", database="heroku_642c138889636e7")
         conn.text_factory = str
         cur = conn.cursor()
-        print("Connexion reussie à SQLite")
         sql = "INSERT INTO connexion (Identifiant, Password) VALUES (%s,%s)"
         value = (identifiant, pw_hash)
         cur.execute(sql, value)
         conn.commit()
-        print("Fichier insere avec succes")
         cur.close()
         conn.close()
-        print("Connexion SQLite est fermee")
 
     except mysql.connector.Error as error:
         print("Erreur lors de l'insertion", error)
     
     return render_template("validation.html")
 
+
+# Page affichant la liste des étudiants en affichant une seule année à la fois
 @app.route('/annee', methods=['POST'])
 def annee():
     if(request.headers.get("X-Forwarded-For") in session ):
@@ -163,7 +168,7 @@ def annee():
             conn = mysql.connector.connect(host="eu-cdbr-west-01.cleardb.com", user="bc534e43745e55", password="3db62771", database="heroku_642c138889636e7")
             conn.text_factory = str
             cur = conn.cursor()
-            print("Connexion reussie à SQLite")
+            # On effectue une verification pour les années 4 et 5 car il n'y a pas de 4 ou de 5 pour identifier l'année en BDD
             if annees[0] == '4':
                 cur.execute("SELECT * FROM Etudiant WHERE (Promo = 'M1')")
 
@@ -180,7 +185,7 @@ def annee():
         return redirect('/')
     
     
-
+# Page affichant la liste des étudiants en affichant une seule promotion à la fois
 @app.route('/promo', methods=['POST'])
 def promo():
     try :
@@ -191,7 +196,6 @@ def promo():
             conn = mysql.connector.connect(host="eu-cdbr-west-01.cleardb.com", user="bc534e43745e55", password="3db62771", database="heroku_642c138889636e7")
             conn.text_factory = str
             cur = conn.cursor()
-            print("Connexion reussie à SQLite")
             cur.execute("SELECT * FROM Etudiant WHERE (Promo = '" + promo + "')")
             posts = cur.fetchall()
             return render_template('promo.html', posts = posts, promo = promos)
@@ -202,37 +206,37 @@ def promo():
     except mysql.connector.Error as error:
         print("Erreur lors de l'insertion", error)
 
+# Page affichant un graphique montrant la porportion entre les élèves présents et absents
 @app.route('/graphes')
 def graphes():
     if(request.headers.get("X-Forwarded-For") in session ):
         conn = mysql.connector.connect(host="eu-cdbr-west-01.cleardb.com", user="bc534e43745e55", password="3db62771", database="heroku_642c138889636e7")
         conn.text_factory = str
         cur = conn.cursor()
-        print("Connexion reussie à SQLite")
         cur.execute("SELECT * FROM Etudiant")
         posts = cur.fetchall()
-        cur.execute("SELECT COUNT(*) FROM Etudiant WHERE Presence = '%'")
-        presences = cur.fetchone()[0]
-        cur.execute("SELECT COUNT(*) FROM Etudiant WHERE Presence = '$'")
+        cur.execute("SELECT COUNT(*) FROM Etudiant WHERE Presence = '%'")# On récupère le nombre d'élèves présents en ce moment
+        presences = cur.fetchone()[0]# On l'ajoute à notre tableau 
+        cur.execute("SELECT COUNT(*) FROM Etudiant WHERE Presence = '$'")# On récupère le nombre d'élèves absents en ce moment
         absences = cur.fetchone()[0]
         cur.close()
         conn.close()
-        print("Connexion SQLite est fermee")
         try:
-            pctPre = round(presences/(presences + absences) * 100, 2)
-            pctAbs = round(absences/(presences + absences) * 100, 2)
+            pctPre = round(presences/(presences + absences) * 100, 2)# On calcule le pourcentage d'élèves présents en arrondissant au centième
+            pctAbs = round(absences/(presences + absences) * 100, 2)# On calcule le pourcentage d'élèves absents en arrondissant au centième
             pct = []
             pct.append(pctPre)
             pct.append(pctAbs)
             return render_template('graphes.html', posts = posts, presences = presences, absences = absences, pct = pct)
         except:
-            print("error")
+            print("erreur division par zero")
             return redirect('/index') 
 
 
     else:
         return redirect('/') 
 
+# Page nous servant à supprimer un étudiant de la BDD
 @app.route('/supprimer')
 def pageSupprimer():
     if (request.headers.get("X-Forwarded-For") in session) :
@@ -240,6 +244,7 @@ def pageSupprimer():
     else:
         return redirect('/')
 
+# Page de validation, de vérification et qui effectue la suppression de l'étudiant
 @app.route('/validSupr', methods=['POST'])
 def supprimer():
     if (request.headers.get("X-Forwarded-For") in session) :
@@ -248,31 +253,23 @@ def supprimer():
             nom = request.form['nom']
             prenom = request.form['prenom']
             promo = request.form['promo']
-            print(nom)
-            print(prenom)
-            print(promo)
             validations.append(nom)
             validations.append(prenom)
             validations.append(promo)
             conn = mysql.connector.connect(host="eu-cdbr-west-01.cleardb.com", user="bc534e43745e55", password="3db62771", database="heroku_642c138889636e7")
             conn.text_factory = str
             cur = conn.cursor()
-            print("Connexion reussie à SQLite")
             cur.execute("SELECT id FROM etudiant WHERE Nom = '" + nom + "' AND Prenom = '" + prenom + "' AND Promo = '" + promo + "'")
             id = cur.fetchone()
             if(id!=None):
-                print(id[0])
                 idstr = str(id[0])
                 cur.execute("DELETE FROM etudiant WHERE id = '" + idstr + "'")
                 conn.commit()
-                print("Fichier supprimé avec succes")
                 cur.close()
                 conn.close()
-                print("Connexion SQLite est fermee")
             else:
                 cur.close()
                 conn.close()
-                print("Connexion SQLite est fermee")
                 return render_template('IncorrectSuppr.html')
 
         except mysql.connector.Error as error:
@@ -283,6 +280,7 @@ def supprimer():
     else:
         return redirect('/') 
 
+# Page de paramètres nous permettant de modifier le mot de passe de la session courante
 @app.route('/settings')
 def settings():
     if(request.headers.get("X-Forwarded-For")  in session):
@@ -290,27 +288,23 @@ def settings():
     else:
         return redirect('/')
 
+# Page de modification effective du mot de passe
 @app.route('/newMDP', methods=['POST'])
 def newMDP():
     if(request.headers.get("X-Forwarded-For")  in session):
         try:
-            print("ON EST LA 1")
             identifiant = request.form['identifiant']
             if(identifiant == session[request.headers.get("X-Forwarded-For")]):
-                print("ON EST LA2")
                 ancienMDP = request.form['ancienMDP']
                 nouveauMDP = request.form['nouveauMDP']
                 conn = mysql.connector.connect(host="eu-cdbr-west-01.cleardb.com", user="bc534e43745e55", password="3db62771", database="heroku_642c138889636e7")
                 conn.text_factory = str
                 cur = conn.cursor()
-                print("Connexion reussie à SQLite")
                 cur.execute("SELECT Password FROM connexion WHERE Identifiant = '" + identifiant + "'")
                 password = cur.fetchone()
-                print(password)
+                # On compare notre mot de passe en BDD avec notre ancien mot de passe du formulaire en utilisant la méthode donnée par BCrypt
                 if(bcrypt.check_password_hash(password[0], ancienMDP)):
-                    print("ON EST LA3")
                     pw_hash = bcrypt.generate_password_hash(nouveauMDP)
-                    print('verif')
                     cur.execute("DELETE FROM connexion WHERE Identifiant = '" + identifiant + "'")
                     conn.commit()
                     sql = "INSERT INTO connexion (Identifiant, Password) VALUES (%s,%s)"
@@ -322,7 +316,6 @@ def newMDP():
                     return redirect('/settings')
                 cur.close()
                 conn.close()
-                print("Connexion SQLite est fermee")
                 return redirect('/index')
             return redirect('/settings')
         except mysql.connector.Error as error:
