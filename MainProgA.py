55dd19ddbf08d13df913337e08c1e6f474216bec
Abdel <abderzak.kaddouri@isen.yncrea.fr>
2021-06-15 17:02:52 +0200

    Derniere Version

---
+++ MainProgA.py
@@ -0,0 +1,6 @@
+import os
+
+os.system("python bdd_extraction.py")
+os.system("python encodage_faces.py --i bdd_picture -e encodings.pickle")
+os.system("python recognition_faces_video_absent.py -e encodings.pickle --display 1")
+#Nous permet de lancer grâce à des commandes systèmes tous les script dans le bonne ordre
--- MainProgP.py
+++ MainProgP.py
@@ -2,6 +2,5 @@ import os
 
 os.system("python bdd_extraction.py")
 os.system("python encodage_faces.py --i bdd_picture -e encodings.pickle")
-os.system("python recognition_faces_video.py -e encodings.pickle --display 1")
-
+os.system("python recognition_faces_video_present.py -e encodings.pickle --display 1")
 #Nous permet de lancer grâce à des commandes systèmes tous les script dans le bonne ordre
